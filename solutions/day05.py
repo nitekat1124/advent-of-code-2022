19bdf81af23c500630fc5fce03b73035d0999ba3
nitekat1124 <nitekat1124@gmail.com>
2022-12-05 13:57:18 +0800

    Day 05

---
+++ solutions/day05.py
@@ -0,0 +1,82 @@
+from utils.solution_base import SolutionBase
+
+
+class Solution(SolutionBase):
+    def solve(self, part_num: int):
+        self.test_runner(part_num)
+
+        func = getattr(self, f"part{part_num}")
+        result = func(self.data)
+        return result
+
+    def test_runner(self, part_num):
+        test_inputs = self.get_test_input()
+        test_results = self.get_test_result(part_num)
+        test_counter = 1
+
+        func = getattr(self, f"part{part_num}")
+        for i, r in zip(test_inputs, test_results):
+            if len(r):
+                if (tr := str(func(i))) == r[0]:
+                    print(f"test {test_counter} passed")
+                else:
+                    print(f"your result: {tr}")
+                    print(f"test answer: {r[0]}")
+                    print(f"test {test_counter} NOT passed")
+            test_counter += 1
+        print()
+
+    def check_is_raw(self):
+        if self.is_raw is False:
+            print("please use --raw flag in this puzzle")
+            exit()
+
+    def part1(self, data):
+        self.check_is_raw()
+
+        sep = [i for i, v in enumerate(data) if v == ""][0]
+
+        crates = data[: sep - 1][::-1]
+        procedures = data[sep + 1 :]
+
+        stack_num = max(map(int, data[sep - 1].split()))
+        stacks = [[] for _ in range(stack_num + 1)]
+
+        for line in crates:
+            items = [line[i] for i in range(1, len(line), 4)]
+            for i, v in enumerate(items):
+                if v != " ":
+                    stacks[i + 1].append(v)
+
+        for line in procedures:
+            _, a, _, b, _, c = [int(v) if i % 2 else v for i, v in enumerate(line.split())]
+            stacks[c].extend(stacks[b][-a:][::-1])
+            # stacks[b] = stacks[b][:-a]
+            del stacks[b][-a:]
+
+        return "".join(stacks[i][-1] for i in range(1, stack_num + 1))
+
+    def part2(self, data):
+        self.check_is_raw()
+
+        sep = [i for i, v in enumerate(data) if v == ""][0]
+
+        crates = data[: sep - 1][::-1]
+        procedures = data[sep + 1 :]
+
+        stack_num = max(map(int, data[sep - 1].split()))
+        stacks = [[] for _ in range(stack_num + 1)]
+
+        for line in crates:
+            items = [line[i] for i in range(1, len(line), 4)]
+            for i, v in enumerate(items):
+                if v != " ":
+                    stacks[i + 1].append(v)
+
+        for line in procedures:
+            _, a, _, b, _, c = [int(v) if i % 2 else v for i, v in enumerate(line.split())]
+            stacks[c].extend(stacks[b][-a:])
+            # stacks[b] = stacks[b][:-a]
+            del stacks[b][-a:]
+
+        return "".join(stacks[i][-1] for i in range(1, stack_num + 1))
