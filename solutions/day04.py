604f037223827c9514880bb002daf084d8661ca2
nitekat1124 <nitekat1124@gmail.com>
2022-12-05 19:22:39 +0800

    move Solution.solve(), Solution.test_runner() and Solution.check_is_raw() to SolutionBase

--- solutions/day04.py
+++ solutions/day04.py
@@ -2,30 +2,6 @@ from utils.solution_base import SolutionBase
 
 
 class Solution(SolutionBase):
-    def solve(self, part_num: int):
-        self.test_runner(part_num)
-
-        func = getattr(self, f"part{part_num}")
-        result = func(self.data)
-        return result
-
-    def test_runner(self, part_num):
-        test_inputs = self.get_test_input()
-        test_results = self.get_test_result(part_num)
-        test_counter = 1
-
-        func = getattr(self, f"part{part_num}")
-        for i, r in zip(test_inputs, test_results):
-            if len(r):
-                if (tr := str(func(i))) == r[0]:
-                    print(f"test {test_counter} passed")
-                else:
-                    print(f"your result: {tr}")
-                    print(f"test answer: {r[0]}")
-                    print(f"test {test_counter} NOT passed")
-            test_counter += 1
-        print()
-
     def part1(self, data):
         r = 0
         for line in data:
--- utils/solution_base.py
+++ utils/solution_base.py
@@ -12,3 +12,32 @@ class SolutionBase:
 
     def get_test_result(self, part_num):
         return PuzzleReader.get_test_result(self.day_num, part_num)
+
+    def solve(self, part_num: int):
+        self.test_runner(part_num)
+
+        func = getattr(self, f"part{part_num}")
+        result = func(self.data)
+        return result
+
+    def test_runner(self, part_num):
+        test_inputs = self.get_test_input()
+        test_results = self.get_test_result(part_num)
+        test_counter = 1
+
+        func = getattr(self, f"part{part_num}")
+        for i, r in zip(test_inputs, test_results):
+            if len(r):
+                if (tr := str(func(i))) == r[0]:
+                    print(f"test {test_counter} passed")
+                else:
+                    print(f"your result: {tr}")
+                    print(f"test answer: {r[0]}")
+                    print(f"test {test_counter} NOT passed")
+            test_counter += 1
+        print()
+
+    def check_is_raw(self):
+        if self.is_raw is False:
+            print("please use --raw flag in this puzzle")
+            exit()
