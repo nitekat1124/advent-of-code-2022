46463e6e743c17a3533f4e2e935027e694b45141
nitekat1124 <nitekat1124@gmail.com>
2022-12-07 20:44:12 +0800

    Day 01 refactor

--- solutions/day01.py
+++ solutions/day01.py
@@ -3,9 +3,12 @@ from utils.solution_base import SolutionBase
 
 class Solution(SolutionBase):
     def part1(self, data):
-        items = [sum(map(int, elf.split("\n"))) for elf in "\n".join(data).split("\n\n")]
+        items = self.parse_items(data)
         return max(items)
 
     def part2(self, data):
-        items = [sum(map(int, elf.split("\n"))) for elf in "\n".join(data).split("\n\n")]
+        items = self.parse_items(data)
         return sum(sorted(items)[-3:])
+
+    def parse_items(self, data):
+        return [sum(map(int, elf.split("\n"))) for elf in "\n".join(data).split("\n\n")]
