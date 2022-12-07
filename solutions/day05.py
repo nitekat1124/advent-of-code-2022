df5d1c4ecda5f688b9330352fd426c9e55ef11b8
nitekat1124 <nitekat1124@gmail.com>
2022-12-07 20:56:24 +0800

    Day 05 refactor

--- solutions/day05.py
+++ solutions/day05.py
@@ -5,31 +5,16 @@ class Solution(SolutionBase):
     def part1(self, data):
         self.check_is_raw()
 
-        sep = [i for i, v in enumerate(data) if v == ""][0]
-
-        crates = data[: sep - 1][::-1]
-        procedures = data[sep + 1 :]
-
-        stack_num = max(map(int, data[sep - 1].split()))
-        stacks = [[] for _ in range(stack_num + 1)]
-
-        for line in crates:
-            items = [line[i] for i in range(1, len(line), 4)]
-            for i, v in enumerate(items):
-                if v != " ":
-                    stacks[i + 1].append(v)
-
-        for line in procedures:
-            _, move, _, _from, _, to = [int(v) if i % 2 else v for i, v in enumerate(line.split())]
-            stacks[to].extend(stacks[_from][-move:][::-1])
-            # stacks[_from] = stacks[_from][:-move]
-            del stacks[_from][-move:]
-
-        return "".join(stacks[i][-1] for i in range(1, stack_num + 1))
+        stacks = self.exec_procedures(data)
+        return "".join(stacks[i][-1] for i in range(1, len(stacks)))
 
     def part2(self, data):
         self.check_is_raw()
 
+        stacks = self.exec_procedures(data, multiple=True)
+        return "".join(stacks[i][-1] for i in range(1, len(stacks)))
+
+    def exec_procedures(self, data, multiple=False):
         sep = [i for i, v in enumerate(data) if v == ""][0]
 
         crates = data[: sep - 1][::-1]
@@ -46,8 +31,11 @@ class Solution(SolutionBase):
 
         for line in procedures:
             _, move, _, _from, _, to = [int(v) if i % 2 else v for i, v in enumerate(line.split())]
-            stacks[to].extend(stacks[_from][-move:])
+            moving_crates = stacks[_from][-move:]
+            if not multiple:
+                moving_crates = moving_crates[::-1]
+            stacks[to].extend(moving_crates)
             # stacks[_from] = stacks[_from][:-move]
             del stacks[_from][-move:]
 
-        return "".join(stacks[i][-1] for i in range(1, stack_num + 1))
+        return stacks
