0a760254128b52f2c5553d18661195f41cf42428
nitekat1124 <nitekat1124@gmail.com>
2022-12-07 21:00:21 +0800

    Day 06 refactor

--- solutions/day06.py
+++ solutions/day06.py
@@ -3,13 +3,13 @@ from utils.solution_base import SolutionBase
 
 class Solution(SolutionBase):
     def part1(self, data):
-        buffer = data[0]
-        for i in range(4, len(buffer)):
-            if len(set(buffer[:i][-4:])) == 4:
-                return i
+        return self.find_marker(data, 4)
 
     def part2(self, data):
+        return self.find_marker(data, 14)
+
+    def find_marker(self, data, size):
         buffer = data[0]
-        for i in range(14, len(buffer)):
-            if len(set(buffer[:i][-14:])) == 14:
+        for i in range(size, len(buffer)):
+            if len(set(buffer[:i][-size:])) == size:
                 return i
