e6cbaab98e459c7d4199d4500ad2478d9080134c
nitekat1124 <nitekat1124@gmail.com>
2022-12-14 17:42:46 +0800

    Day 14, rewrite the solution to improve the performance

--- solutions/day14.py
+++ solutions/day14.py
@@ -1,87 +1,62 @@
 from utils.solution_base import SolutionBase
-from collections import defaultdict
-
-"""
-0: air (.)
-1: rock (#)
-2: sand (+)
-3: rested sand (o)
-"""
 
 
 class Solution(SolutionBase):
     def part1(self, data):
-        sand = (500, 0)
+        sand_pos = (500, 0)
 
-        _map = defaultdict(int)
-        _map[sand] = 2
+        rock = self.parse_data(data)
+        rest = set()
 
-        for line in data:
-            coords = [tuple(map(int, i.split(","))) for i in line.split(" -> ")]
-            for i in range(len(coords) - 1):
-                if coords[i][0] == coords[i + 1][0]:
-                    a, b = coords[i][1], coords[i + 1][1]
-                    if a > b:
-                        a, b = b, a
-                    for j in range(a, b + 1):
-                        _map[(coords[i][0], j)] = 1
-                else:
-                    a, b = coords[i][0], coords[i + 1][0]
-                    if a > b:
-                        a, b = b, a
-                    for j in range(a, b + 1):
-                        _map[(j, coords[i][1])] = 1
+        max_depth = max(i[1] for i in rock)
+        falls = [(0, 1), (-1, 1), (1, 1)]
 
-        max_y = max([i[1] for i in _map.keys()])
-
-        while sand[1] < max_y:
-            next_sands = [(sand[0], sand[1] + 1), (sand[0] - 1, sand[1] + 1), (sand[0] + 1, sand[1] + 1)]
-            for next_sand in next_sands:
-                if _map[next_sand] == 0:
-                    _map[sand] = 0
-                    _map[next_sand] = 2
-                    sand = next_sand
+        while sand_pos[1] < max_depth:
+            for fall in falls:
+                next_sand_pos = (sand_pos[0] + fall[0], sand_pos[1] + fall[1])
+                if next_sand_pos not in rock and next_sand_pos not in rest:
+                    sand_pos = next_sand_pos
                     break
             else:
-                _map[sand] = 3
-                sand = (500, 0)
+                rest.add(sand_pos)
+                sand_pos = (500, 0)
 
-        return list(_map.values()).count(3)
+        return len(rest)
 
     def part2(self, data):
-        sand = (500, 0)
+        sand_pos = (500, 0)
 
-        _map = defaultdict(int)
-        _map[sand] = 2
+        rock = self.parse_data(data)
+        rest = set()
+
+        ground = max(i[1] for i in rock) + 2
+        falls = [(0, 1), (-1, 1), (1, 1)]
+
+        while (500, 0) not in rest:
+            for fall in falls:
+                next_sand_pos = (sand_pos[0] + fall[0], sand_pos[1] + fall[1])
+                if next_sand_pos not in rock and next_sand_pos not in rest and next_sand_pos[1] < ground:
+                    sand_pos = next_sand_pos
+                    break
+            else:
+                rest.add(sand_pos)
+                sand_pos = (500, 0)
+
+        return len(rest)
+
+    def parse_data(self, data):
+        rock = set()
 
         for line in data:
             coords = [tuple(map(int, i.split(","))) for i in line.split(" -> ")]
             for i in range(len(coords) - 1):
                 if coords[i][0] == coords[i + 1][0]:
-                    a, b = coords[i][1], coords[i + 1][1]
-                    if a > b:
-                        a, b = b, a
+                    a, b = sorted([coords[i][1], coords[i + 1][1]])
                     for j in range(a, b + 1):
-                        _map[(coords[i][0], j)] = 1
+                        rock.add((coords[i][0], j))
                 else:
-                    a, b = coords[i][0], coords[i + 1][0]
-                    if a > b:
-                        a, b = b, a
+                    a, b = sorted([coords[i][0], coords[i + 1][0]])
                     for j in range(a, b + 1):
-                        _map[(j, coords[i][1])] = 1
+                        rock.add((j, coords[i][1]))
 
-        ground = max([i[1] for i in _map.keys()]) + 2
-
-        while _map[(500, 0)] != 3:
-            next_sands = [(sand[0], sand[1] + 1), (sand[0] - 1, sand[1] + 1), (sand[0] + 1, sand[1] + 1)]
-            for next_sand in next_sands:
-                if next_sand[1] < ground and _map[next_sand] == 0:
-                    _map[sand] = 0
-                    _map[next_sand] = 2
-                    sand = next_sand
-                    break
-            else:
-                _map[sand] = 3
-                sand = (500, 0)
-
-        return list(_map.values()).count(3)
+        return rock
